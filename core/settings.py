d8a9071ba281d4eaf98b911731097b5b5e0c1c32
mefmylv <129817583+mefmylv@users.noreply.github.com>
2024-02-08 20:53:33 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -93,8 +93,8 @@ DATABASES = {
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_USER_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
